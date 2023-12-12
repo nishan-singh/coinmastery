c864f4f6ca293cd98fd41ff68709f60409e9c62c
Nishan Singh <singh007nishan@gmail.com>
2023-12-12 12:15:59 +0100

    Refactor routing and component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
-import { Component } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { RouterOutlet } from "@angular/router";
-import { TopBarComponent } from "./components/top-bar/top-bar.component";
-import { HeaderComponent } from "./components/header/header.component";
-
-@Component({
-  selector: "app-root",
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, TopBarComponent, HeaderComponent],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
-})
-export class AppComponent {
-=======
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, TopBarComponent, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'coinmastery';
->>>>>>> d478c1b7676e583b53b7ab72f015b850d6048769
-}
+export class AppComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-=======
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
->>>>>>> d478c1b7676e583b53b7ab72f015b850d6048769
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
